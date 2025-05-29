f97956338040c55988d75bf5a6b36d352713948f
IrfanAlam <irfancs176@gmail.com>
2025-05-29 21:27:37 +0500

    reset all things

--- model/db.js
+++ model/db.js
@@ -1,22 +1,13 @@
 // db.js
 import mysql from "mysql2/promise";
 
-// const db = await mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "beautifydb"
-// });
-
-// export default db;
-
-
-
 const db = await mysql.createConnection({
-  host: "sql.freedb.tech",
-  user: "freedb_beautifydbuser",
-  password: "WU$SRww9Z#?Jrys",
-  database: "freedb_beautifydb"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "beautifydb"
 });
 
 export default db;
+
+
